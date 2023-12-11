b2b0872e351eebe4df41b8e321f99f9536542c6a
Magomed Bekkaev <bekkaev.magomed@gmail.com>
2023-12-11 14:02:12 +0100

    symfony

---
+++ src/Controller/EmployeController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Employe;
+use App\Repository\EmployeRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class EmployeController extends AbstractController
+{
+    #[Route('/employe', name: 'app_employe')]
+    public function index(employeRepository $employeRepository): Response
+    {
+        // $employes = $employeRepository->findAll();
+        // SELECT * FROM employe ORDER BY nom ASC
+        $employes = $employeRepository->findBy([], ['nom' => 'ASC']);
+        return $this->render('employe/index.html.twig', [
+            'employes' => $employes
+        ]);
+    }
+
+    #[Route('/employe/{id}', name: 'show_employe')]
+    public function show(Employe $employe): Response
+    {
+        return $this->render('employe/show.html.twig', [
+            'employe' => $employe
+        ]);
+
+    }
+}
