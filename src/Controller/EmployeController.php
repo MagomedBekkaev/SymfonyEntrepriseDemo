94e71f2241f385745095dd77b0e5b6a26b3fd38b
Magomed Bekkaev <bekkaev.magomed@gmail.com>
2023-12-19 10:49:04 +0100

    rejout de fonctionnalités

--- src/Controller/EmployeController.php
+++ src/Controller/EmployeController.php
@@ -3,13 +3,17 @@
 namespace App\Controller;
 
 use App\Entity\Employe;
+use App\Form\EmployeType;
 use App\Repository\EmployeRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class EmployeController extends AbstractController
 {
+
     #[Route('/employe', name: 'app_employe')]
     public function index(employeRepository $employeRepository): Response
     {
@@ -21,12 +25,37 @@ class EmployeController extends AbstractController
         ]);
     }
 
-    #[Route('/employe/{id}', name: 'show_employe')]
-    public function show(Employe $employe): Response
+    #[Route('/employe/new', name: 'new_employe')]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
-        return $this->render('employe/show.html.twig', [
-            'employe' => $employe
-        ]);
+        $employe = new Employe();
 
+        $form = $this->createForm(EmployeType::class, $employe);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            
+            $employe = $form->getData();
+            //prepare PDO
+            $entityManager->persist($employe);
+            //execute PDO
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_employe');
+        }
+
+        return $this->render('employe/new.html.twig', [
+            'formAddEmploye' => $form,
+        ]);
     }
+
+    #[Route('/employe/{id}', name: 'show_employe')]
+        public function show(Employe $employe): Response
+        {
+            return $this->render('employe/show.html.twig', [
+                'employe' => $employe
+            ]);
+            
+        }
 }
